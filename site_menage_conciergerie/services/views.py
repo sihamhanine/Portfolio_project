7fe13ba4fdcba4990857dcd342e030d30791b6a3
sihamelani <siham.elani17@hotmail.com>
2024-10-08 13:33:43 +0200

    Task0

--- site_menage_conciergerie/services/views.py
+++ site_menage_conciergerie/services/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Service
 
 def accueil(request):
     return render(request, 'front_end/accueil.html')  # Affiche le template accueil.html
+
+def nos_services(request):
+    services = Service.objects.all()  # Récupérer tous les services depuis la base de données
+    return render(request, 'front_end/NosServices.html', {'services': services})
 
--- site_menage_conciergerie/site_menage_conciergerie/urls.py
+++ site_menage_conciergerie/site_menage_conciergerie/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from services import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.accueil, name='accueil'),  # Page d'accueil
+    path('nosservices/', views.nos_services, name='nos_services'),
 ]
+
