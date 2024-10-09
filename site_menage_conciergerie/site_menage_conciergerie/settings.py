689eabdb6ea5ea410ed4f0099be5408cee422b93
sihamelani <siham.elani17@hotmail.com>
2024-10-09 11:46:04 +0200

    delete some images

--- site_menage_conciergerie/site_menage_conciergerie/settings.py
+++ site_menage_conciergerie/site_menage_conciergerie/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'services',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- site_menage_conciergerie/site_menage_conciergerie/urls.py
+++ site_menage_conciergerie/site_menage_conciergerie/urls.py
@@ -3,11 +3,40 @@ from django.urls import path, include
 from services import views
 from django.conf import settings
 from django.conf.urls.static import static
+from services.views import (
+    ClientList, ClientDetail,
+    ServiceList, ServiceDetail,
+    ReservationList, ReservationDetail,
+    DevisList, DevisDetail,
+    ContactList, ContactDetail,
+    nos_services,
+)
 
 
 urlpatterns = [
+    # Point de terminaison pour l'API
+    path('api/clients/', ClientList.as_view(), name='client-list'),
+    path('api/clients/<int:pk>/', ClientDetail.as_view(), name='client-detail'),
+
+    path('api/services/', ServiceList.as_view(), name='service-list'),
+    path('api/services/<int:pk>/', ServiceDetail.as_view(), name='service-detail'),
+
+    path('api/reservations/', ReservationList.as_view(), name='reservation-list'),
+    path('api/reservations/<int:pk>/', ReservationDetail.as_view(), name='reservation-detail'),
+
+    path('api/devis/', DevisList.as_view(), name='devis-list'),
+    path('api/devis/<int:pk>/', DevisDetail.as_view(), name='devis-detail'),
+
+    path('api/contacts/', ContactList.as_view(), name='contact-list'),
+    path('api/contacts/<int:pk>/', ContactDetail.as_view(), name='contact-detail'),
+
+    #  Vues pour les pages HTML
     path('admin/', admin.site.urls),
     path('', views.accueil, name='accueil'),  # Page d'accueil
     path('nosservices/', views.nos_services, name='nos_services'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
